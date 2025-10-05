aec97fbe674548da70e4faeeaabc86676ec14bbd
pedrovaz100 <peddro.vaz.ferreira@gmail.com>
2025-10-04 23:41:00 -0300

    criaçao do conteudo no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+
 export default function App() {
   return (
-    <div style={{textAlign: "center", marginTop: "40px"}}>
-      <h1>Access Control - Projeto Inicial</h1>
-      <p>Projeto base para primeiro commit</p>
-    </div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/home" element={<Home />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
